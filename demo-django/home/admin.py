5819cf620468bdb83155532271bf9cf3f3f25cd5
Gabriel Vilas Novas Sousa <gabriel.novas@aluno.ufop.edu.br>
2026-07-03 01:05:26 -0300

    Roteiro - Projeto Parte 3

--- demo-django/home/admin.py
+++ demo-django/home/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Mensagem
+from .models import Categoria, Mensagem
+
+@admin.register(Categoria)                                       # ← novo
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
 
 
 @admin.register(Mensagem)
 class MensagemAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "criada_em")
+    list_display = ("titulo", "categoria", "criada_em")          # ← + categoria
+    list_filter = ("categoria",)                                 # ← novo filtro
     search_fields = ("titulo", "conteudo")
--- demo-django/home/models.py
+++ demo-django/home/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["nome"]
+
+    def __str__(self):
+        return self.nome
+
 
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
     conteudo = models.TextField()
-    autor = models.CharField(max_length=80, default="Anônimo") 
+    autor = models.CharField(max_length=80, default="Anônimo")
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="mensagens",
+    )
     criada_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
